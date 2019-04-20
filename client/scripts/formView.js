ef524feb35d67ab956cbb9f7df1490b9257b608b
sbtorres <scott.torres27@gmail.com>
2019-04-19 20:54:23 -0700

    Implement submit button and ability to post messages to server

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -2,18 +2,34 @@ var FormView = {
 
   $form: $('form'),
 
-  initialize: function() {
+  initialize: function () {
     FormView.$form.on('submit', FormView.handleSubmit);
   },
 
-  handleSubmit: function(event) {
+  handleSubmit: function (event) {
     // Stop the browser from submitting the form
-    //event.preventDefault();
-    
-    console.log('click!');
+    event.preventDefault();
+
+    var msg = $("input:first").val();
+    var user = window.location.search.substr(10);
+    var roomName = 'Our room';
+    var oneMessage = { username: user, text: msg, roomname: roomName };
+    Parse.create(oneMessage,
+      function (data) {
+        console.log('chatterbox: Message sent');
+        Parse.readAll((data) => {
+          // examine the response from the server request:
+          window.Messages.data = data.results;
+        })
+      },
+      function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      });
+    //location.reload();
   },
 
-  setStatus: function(active) {
+  setStatus: function (active) {
     var status = active ? 'true' : null;
     FormView.$form.find('input[type=submit]').attr('disabled', status);
   }
